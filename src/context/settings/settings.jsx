5f1f07c86bc97aadcac5e169a02ca446cbf29291
ehabsalhi <ehab1salhe@gmail.com>
2023-09-12 09:13:26 +0000

    login to do

--- src/context/settings/settings.jsx
+++ src/context/settings/settings.jsx
@@ -1,12 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { v4 as uuid } from "uuid";
+import { LoginContext } from "./login";
+import axios from "axios";
 
 export const settingsProvider = React.createContext();
 
 export default function Settings(props) {
   let id = 0;
 
-  const [state, setState] = useState([
+ let on = [
     {
       text: "text 1",
       completed: false,
@@ -28,22 +30,36 @@ export default function Settings(props) {
       difficulty: 0,
       id: id++,
     },
-  ]);
+  ]
+  const [state, setState] = useState([]);
   const [final, setFinal] = useState(state.slice(0, 3));
   const [incomplete, setIncomplete] = useState([]);
-  const [settings, setSettings] = useState({ itemsPerPage: 3, difficulty: 0 , showCompleted : true });
+  const [update , setUpdate] = useState([]);
+  const [settings, setSettings] = useState({ itemsPerPage: 3, difficulty: 0, showCompleted: true });
+  const {capabilities} = useContext(LoginContext)
 
-     function toggleComplete(id) {
-     const items = state.map((item) => {
-          if (item.id === id) {
-          item.completed = !item.completed;
-          }
-          return item;
-     });
 
-     setState(items);
-     }
-     
+   function toggleComplete(id) {
+    if (capabilities.includes('update')) {
+         
+      const items =  state.map( async (item) => {
+           if (item.id === id) {
+             item.completed = !item.completed;
+             try {
+              item.id = id
+               const res = await axios.put(`https://auth-api-fz5h.onrender.com/todo/${id}`, item)
+               setUpdate(res.data.data)
+             } catch (err) {
+               console.log('update error' . err);
+             }
+           }
+           return item;
+      });
+
+      // setState(items);
+    }
+   }
+
      function change(number) {
           let localStorageData = JSON.parse(localStorage.getItem('settings'))
 
@@ -57,18 +73,45 @@ export default function Settings(props) {
           }
      }
 
-     function addItem(item) {
-     item.id = uuid();
-     item.completed = false;
-     console.log(item);
-     setState([...state, item]);
+  async function addItem(item) {
+    console.log(item);
+    try {
+      item.completed = false;
+      const res  = await axios.post(`https://auth-api-fz5h.onrender.com/todo` , item)
+         
+       setState([...state, res.data.data]);
+    } catch (err) {
+      console.log('post' , err);
+    }
      }
+  
 
-     function deleteItem(id) {
-     const items = state.filter((item) => item.id !== id);
-     setState(items);
-     }
-     
+  async function deleteItem(id) {
+    if (capabilities.includes('delete')) {
+      try {
+         await axios.delete(`https://auth-api-fz5h.onrender.com/todo/${id}`)
+      } catch (err) {
+        console.log('delete error');
+      }
+
+    const items = state.filter((item) => item.id !== id);
+    setState(items);
+    }
+  }
+  
+  async function getData() {
+    try {
+      const res = await axios.get('https://auth-api-fz5h.onrender.com/todo')
+      console.log(res.data.data);
+      setState(res.data.data)
+    } catch (err) {
+      console.log(err);
+    }
+  }
+   
+  useEffect(() => {
+    getData()
+  },[])
      
 
   let values = {
@@ -81,8 +124,9 @@ export default function Settings(props) {
      incomplete,
      setIncomplete,
      setSettings,
-     settings
-     };
+    settings,
+    deleteItem
+};
 
   return (
     <settingsProvider.Provider value={values}>
